2702a478338f889500d7d1355e1a19fbb8fee716
REFUGIO DAVID TORRES TRUJILLO <refugio.torres2941@alumnos.udg.mx>
2021-06-09 21:07:11 -0500

    Se hacen alertas bacanas

--- php/eliminar-cancion.php
+++ php/eliminar-cancion.php
@@ -2,13 +2,15 @@
    include 'conexion.php';
 
     $id=$_POST['id'];
+    $archivo =$_POST['archivo2'];
     $eliminar =  mysqli_query($conexion, "DELETE FROM cancion WHERE id = '$id'");
-    
-    if($eliminar){
-        echo "eliminao";
+    $archivo = "../musica/".$archivo;    
+    if(unlink($archivo) && $eliminar){
+        echo 'Musica Eliminada';
     }
-    else{
-        echo "no eliminao";
+    else
+    {
+        echo'Error al eliminar';
     }
 
 ?>
