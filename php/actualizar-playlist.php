7e724868551319389788bac0a117bee34af136ef
Nestor RV <nestor.regalado1453@alumnos.udg.mx>
2021-06-18 22:57:32 -0500

    Se agrega correctamente las canciones a playlist

--- php/actualizar-playlist.php
+++ php/actualizar-playlist.php
@@ -4,10 +4,12 @@
     $playlist = $_POST['playlist']; //obtenemos el dato del select con name = "playlist"
     $idCancion = $_POST['idSong'] ; //obtenemos el dato  con name = "idCancion"
 
-    $query = "SELECT * FROM playlist_cancion WHERE idCancion='$idCancion' and idPlaylist='$playlist'";
+    $query = "SELECT * FROM playlist_cancion WHERE idCancion = '$idCancion' and idPlaylist = '$playlist'";
     $revisar = mysqli_query($conexion, $query); ///mandamos la conexion junto al SELECT; 
 
-    if(!$revisar){ // La cancion no esta en la playlist
+    if(mysqli_num_rows($revisar) > 0){ // La cancion esta en la playlist
+        echo"Ya se encuentra en la playlist";
+    }else{ // La cancion no esta en la playlist
         if($playlist != "Agregar Playlist") // No es el valor por defecto
         {
             $query = "INSERT INTO playlist_cancion(idCancion,idPlaylist) VALUES('$idCancion','$playlist')";
@@ -17,8 +19,6 @@
                 echo"Cancion agregada a la playlist"; //mandara este mensaje avisando que la playlist fue actualizada
             }
         }
-    }else{ // La cancion esta en la playlist
-        echo"Ya se encuentra en la playlist";
     }
 
     mysqli_close($conexion); //Cerramos la conexion con BD
